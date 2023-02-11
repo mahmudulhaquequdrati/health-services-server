3a07dfa6297451192a564d99c3261e22a1ee6979
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-02-12 00:31:11 +0600

    updated

--- index.js
+++ index.js
@@ -29,11 +29,14 @@ admin.initializeApp({
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkswl.mongodb.net/your-health-treatment?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const client = new MongoClient(
+  uri
+  //   , {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   serverApi: ServerApiVersion.v1,
+  // }
+);
 
 async function verifyToken(req, res, next) {
   if (req?.headers?.authorization) {
@@ -68,167 +71,201 @@ async function run() {
 
     // Get appointment
     app.get("/appointments", async (req, res) => {
-      const query = await appointments.find({});
-      const result = await query.toArray();
-      res.send(result);
+      try {
+        const query = await appointments.find({});
+        const result = await query.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // Get all users appointments
     app.get("/userAppointments", async (req, res) => {
-      const query = await orders.find({});
-      const result = await query.toArray();
-      res.send(result);
+      try {
+        const query = await orders.find({});
+        const result = await query.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // Get an user total appointment
     app.get("/userTotalAppointment", async (req, res) => {
-      const email = req.query.email;
-      const query = { patientEmail: email };
-      const userAppointments = await orders.find(query);
-      const result = await userAppointments.toArray();
-      res.send(result);
+      try {
+        const email = req.query.email;
+        const query = { patientEmail: email };
+        const userAppointments = await orders.find(query);
+        const result = await userAppointments.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     //Get today's appointment
     app.get("/todaysAppointment", async (req, res) => {
-      const date = new Date(req.query.date);
-      const query = { date: date.toLocaleDateString() };
-      const userAppointments = await orders.find(query);
-      const result = await userAppointments.toArray();
-      res.send(result);
+      try {
+        const date = new Date(req.query.date);
+        const query = { date: date.toLocaleDateString() };
+        const userAppointments = await orders.find(query);
+        const result = await userAppointments.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // Get all Patients //user
     app.get("/patients", async (req, res) => {
-      const query = await users.find({});
-      const result = await query.toArray();
-      res.send(result);
+      try {
+        const query = await users.find({});
+        const result = await query.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // delete a patient
     app.delete("/patients/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await users.deleteOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await users.deleteOne(query);
+        res.send(result);
+      } catch (error) {}
     });
 
     // Get reviews
     app.get("/reviews", async (req, res) => {
-      const query = await reviews.find({});
-      const result = await query.toArray();
-      res.send(result);
+      try {
+        const query = await reviews.find({});
+        const result = await query.toArray();
+        res.send(result);
+      } catch (error) {}
     });
     // test
 
     // Get blogs
     app.get("/blogs", async (req, res) => {
-      const query = await blogs.find({});
-      const result = await query.toArray();
-      res.send(result);
+      try {
+        const query = await blogs.find({});
+        const result = await query.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // Make an Order
     app.post("/appointments", async (req, res) => {
-      const order = req.body;
-      const result = await orders.insertOne(order);
-      res.send(result);
+      try {
+        const order = req.body;
+        const result = await orders.insertOne(order);
+        res.send(result);
+      } catch (error) {}
     });
 
     // post an user to database
     app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await users.insertOne(user);
-      res.send(result);
+      try {
+        const user = req.body;
+        const result = await users.insertOne(user);
+        res.send(result);
+      } catch (error) {}
     });
 
     // delete appointment
     app.delete("/appointment/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await orders.deleteOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orders.deleteOne(query);
+        res.send(result);
+      } catch (error) {}
     });
 
     // Get userAppointments by email and date
     app.get("/userappointmentsED", verifyToken, async (req, res) => {
-      const email = req.query.email;
-      const date = req.query.date;
-      const query = { patientEmail: email, date: date };
-      const userSpecificAppointments = await orders.find(query);
-      const result = await userSpecificAppointments.toArray();
-      res.send(result);
+      try {
+        const email = req.query.email;
+        const date = req.query.date;
+        const query = { patientEmail: email, date: date };
+        const userSpecificAppointments = await orders.find(query);
+        const result = await userSpecificAppointments.toArray();
+        res.send(result);
+      } catch (error) {}
     });
 
     // make someone admin
     app.put("/users/makeAdmin", verifyToken, async (req, res) => {
-      const user = req.body;
-      const requester = req.decodedEmail;
-      // filtering the requester
-      if (requester) {
-        const requesterAccount = await users.findOne({ email: requester });
-        if (requesterAccount.role === "admin") {
-          const filter = { email: user.email };
-          const updateDoc = { $set: { role: "admin" } };
-          const result = await users.updateOne(filter, updateDoc);
-          res.send(result);
+      try {
+        const user = req.body;
+        const requester = req.decodedEmail;
+        // filtering the requester
+        if (requester) {
+          const requesterAccount = await users.findOne({ email: requester });
+          if (requesterAccount.role === "admin") {
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await users.updateOne(filter, updateDoc);
+            res.send(result);
+          }
+        } else {
+          res
+            .status(403)
+            .json({ message: "you do not have access to make someone admin!" });
         }
-      } else {
-        res
-          .status(403)
-          .json({ message: "you do not have access to make someone admin!" });
-      }
+      } catch (error) {}
     });
 
     // Get admin
     app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await users.findOne(query);
-      let isAdmin = false;
-      if (user?.role === "admin") {
-        isAdmin = true;
-      }
-      res.json({ admin: isAdmin });
+      try {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await users.findOne(query);
+        let isAdmin = false;
+        if (user?.role === "admin") {
+          isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+      } catch (error) {}
     });
 
     // payment related routes
     // Get payment user info
     app.get("/appointment/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await orders.findOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orders.findOne(query);
+        res.send(result);
+      } catch (error) {}
     });
 
     // payment post
     app.post("/create-payment-intent", async (req, res) => {
-      const price = req.body;
-      const amount = parseInt(price.price) * 100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        currency: "usd",
-        amount: amount,
-        automatic_payment_methods: {
-          enabled: true,
-        },
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
+      try {
+        const price = req.body;
+        const amount = parseInt(price.price) * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+          currency: "usd",
+          amount: amount,
+          automatic_payment_methods: {
+            enabled: true,
+          },
+        });
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {}
     });
 
     // update appointments orders info to database
     app.put("/appointment/:id", async (req, res) => {
-      const id = req.params.id;
-      const payment = req.body;
-      const filter = { _id: ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          payment: payment,
-        },
-      };
-      const result = await orders.updateOne(filter, updateDoc);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const payment = req.body;
+        const filter = { _id: ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            payment: payment,
+          },
+        };
+        const result = await orders.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {}
     });
   } finally {
     // client.close();
